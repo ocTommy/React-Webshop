857d50ecdf95e0b68919e48f2a6e3a8d45ed0b2d
ocTommy <tommy.nguyenoc@gmail.com>
2023-11-09 08:29:30 +0100

    added function toggleNavbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,35 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/boxLogo.png";
+import "../styles/Navbar.css";
+import { Reorder } from "@mui/icons-material";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className="navbar">
-      <div className="leftSide">
-        <img src={Logo} alt="Christmas box"></img>
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} alt="Christmas box" />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <button onClick={toggleNavbar}>
+          <Reorder />
+        </button>
       </div>
     </div>
   );
