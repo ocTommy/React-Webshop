6b08b41df32a4a6bd6b2bcef6b0fec4dfd6307da
ocTommy <tommy.nguyenoc@gmail.com>
2023-11-09 10:17:55 +0100

    added data to productsList

---
+++ src/helpers/ProductsList.js
@@ -0,0 +1,30 @@
+export const ProductsList = [
+  {
+    id: 1,
+    name: "Kaizoku",
+    price: 12300,
+    image:
+      "https://www.themarysue.com/wp-content/uploads/2020/11/One-Piece.jpg?resize=1200%2C813?w=1200",
+  },
+  {
+    id: 2,
+    name: "Christmas tree",
+    price: 100,
+    image:
+      "https://www.themarysue.com/wp-content/uploads/2020/11/One-Piece.jpg?resize=1200%2C813?w=1200",
+  },
+  {
+    id: 3,
+    name: "Christmas tree",
+    price: 100,
+    image:
+      "https://www.themarysue.com/wp-content/uploads/2020/11/One-Piece.jpg?resize=1200%2C813?w=1200",
+  },
+  {
+    id: 4,
+    name: "Christmas tree",
+    price: 100,
+    image:
+      "https://www.themarysue.com/wp-content/uploads/2020/11/One-Piece.jpg?resize=1200%2C813?w=1200",
+  },
+];
